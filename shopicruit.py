276dc3cfbf59aec9ffeee0bc250baf7e44b137b3
Chinmaya <patanaikchinmaya@gmail.com>
2016-06-30 10:31:33 +0530

    HTTP connection error exception added.

--- shopicruit.py
+++ shopicruit.py
@@ -2,30 +2,25 @@
 Author - Chinmaya Kr. Patanaik
 Email - patanaikchinmaya@gmail.com
 
-Solution to Shopicruit task for fall internship position.
 
-Third Party libraries used:
-===========================
-* requests
+Source Code - https://github.com/pattu777/Shopify
 """
-
-import sys
 import requests
 import collections
 
 computer_variants = {}
-keyboard_variants = {}
+keyboard_variants = {}    
 URL = "http://shopicruit.myshopify.com/products.json"
 
 
-def find_min_variants(computers, keyboards):
+def find_all_variants(computers, keyboards):
     """
     Returns the final list of products to buy.
-
-    @params computers:   A sorted list of unique computer variant tuples.
-    @paramas keyboards:  A sorted list of unique keyboard variant tuples.
-    @type computers:     list
-    @type keyboards:     list
+    
+    @params computers: A sorted list of unique computer variant tuples.
+    @paramas keyboards: A sorted list of unique keyboard variant tuples.
+    @type computers: list
+    @type keyboards: list
     """
     total_weight = 0
     total_price = 0
@@ -49,28 +44,28 @@ def find_min_variants(computers, keyboards):
             total_weight += computers[index][1]['grams']
             total_price += computers[index][1]['price']
             products.append(computers[index])
-
+        
     print "Total weight - %d" % total_weight
     print "Total price - %d" % total_price
     print "Products - %s" % products
-
+            
 
 def get_products():
     """
     Creates a HTTP request to get all the products in JSON format.
     And then generates a list of unique computers and keyboards and sorts them according to their prices.
     """
-    response = requests.get(url=URL)
-    if response.status_code != requests.codes.ok:
-        print "Unable to send a request"
+    try:
+        response = requests.get(url=URL)
+    except requests.exceptions.ConnectionError:
+        print "Unable to send a request. Please try again after some time."
         return
-    else:
+    if response.status_code == requests.codes.ok:
         products = response.json()['products']
         for item in products:
             if item['product_type'] == 'Keyboard':
                 for variant in item['variants']:
                     if variant['available']:
-
                         # Check if there's a previous entry with more price.
                         if variant['title'] in keyboard_variants:
                             if variant['title'] in keyboard_variants and variant['price'] < keyboard_variants[variant['title']]['price']:
@@ -79,12 +74,11 @@ def get_products():
                                 keyboard_variants[variant['title']]['variant'] = variant
                         else:
                             keyboard_variants[variant['title']] = {'price' : float(variant['price']), 'grams' : variant['grams'], 'variant': variant}
-
+                            
             elif item['product_type'] == 'Computer':
                 for variant in item['variants']:
                     if variant['available']:
                         if variant['title'] in computer_variants:
-
                             # Check if there's a previous entry with more price.
                             if float(variant['price']) < computer_variants[variant['title']]['price']:
                                 computer_variants[variant['title']]['price'] = float(variant['price'])
@@ -92,14 +86,15 @@ def get_products():
                                 computer_variants[variant['title']]['variant'] = variant
                         else:
                             computer_variants[variant['title']] = {'price' : float(variant['price']), 'grams' : variant['grams'], 'variant': variant}
-
-
-        # Sort the computers and keyboards according to their cost.
+                            
+                
+        # Sort the computers and keyboards according to their cost. This will create a list of sorted tuples.
         computers = sorted(computer_variants.iteritems(), key=lambda (x, y): y['price'])
         keyboards = sorted(keyboard_variants.iteritems(), key=lambda (x, y): y['price'])
-        find_min_variants(computers, keyboards)
-
+        find_all_variants(computers, keyboards)
+    else:
+        print "HTTP request was not successful. "
+        
 if __name__ == '__main__':
     get_products()
-    sys.exit(0)
-
+    
